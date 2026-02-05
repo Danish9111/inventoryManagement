394a8170891a5276d329543db255b010e7e59ad3
nadeem danish <nadeemdanish.9188.1@gmail.com>
2026-02-05 15:34:46 +0500

    configuration done

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,20 @@
 const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const connectDB = require("./config/db");
+
+dotenv.config();
+
+connectDB();
+
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json()); // Body parser
 
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+    res.send("API is running...");
 });
 
 app.listen(port, () => {
